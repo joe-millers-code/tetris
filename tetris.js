d714d98b8a2f299255668a8dbcace1cfc4052c0c
github username <Joemiller065@gmail.com>
2020-01-14 19:17:34 -0500

    piece rotations and actual moving pieves functions taking out of the global scope and added to player class

--- tetris.js
+++ tetris.js
@@ -109,26 +109,6 @@ document.addEventListener('keydown', e => {
     }
 })
 
-function rotate(matrix, dir) {
-    for (let y = 0; y < matrix.length; ++y) {
-        for (let x = 0; x < y; ++x) {
-            [
-                matrix[x][y],
-                matrix[y][x]
-            ] = [
-                matrix[y][x],
-                matrix[x][y]
-            ];
-        }
-    }
-
-    if(dir > 0) {
-        matrix.forEach(row => row.reverse());
-    } else {
-        matrix.reverse();
-    }
-}
-
 //-------------------------------------------------Player Mechanics
 
 const player = new Player();
